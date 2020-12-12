1cf09f143a43a8f880a47bd72602748d231328c0
Daniel <daniel.silva@br.experian.com>
2020-12-12 12:18:33 -0300

    Add pytablewriter for format test results

---
+++ format_test_result.py
@@ -0,0 +1,38 @@
+from pytablewriter import MarkdownTableWriter
+import json
+
+
+def intent_table():
+    writer = MarkdownTableWriter()
+    writer.table_name = "Intent Cross-Validation Results"
+
+    with open('results/intent_report.json', 'r') as f:
+        data = json.loads(f.read())
+
+    cols = ["support", "f1-score", "confused_with"]
+    writer.headers = ["class"] + cols
+
+    classes = list(data.keys())
+    classes.remove('accuracy')
+    classes.sort(key=lambda x: data[x]['support'], reverse=True)
+
+    def format_cell(data, c, k):
+        if not data[c].get(k):
+            return "N/A"
+        if k == "confused_with":
+            return ", ".join([f"{k}({v})" for k,v in data[c][k].items()])
+        else:
+            return data[c][k]
+
+    writer.value_matrix = [
+        [c] + [format_cell(data, c, k) for k in cols]
+        for c in classes
+    ]
+
+    return writer.dumps()
+
+
+intents = intent_table()
+
+with open('results.md', 'w') as f:
+    f.write(intents)
